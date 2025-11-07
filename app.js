751d11301a8abcd5934965ccd8ba6304ba8a60ff
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-11-07 09:07:44 -0500

    feat: implement category management with CRUD operations and routing

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 import express from 'express';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import categoryRoutes from './routes/categoryRoutes.js';
 
 const app = express();
 app.disable('x-powered-by');
 
 // Middleware
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// View engine setup
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Routes
-// app.use('/categories', categoryRoutes);
+app.use('/catalog', categoryRoutes);
 // app.use('/items', itemRoutes);
 
 // Home route
