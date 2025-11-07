c737b81a196c1f9e76785606b73d20050eefdb6e
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-11-07 12:11:42 -0500

    refactor: update database connection configuration to support DATABASE_URL

--- db/pool.js
+++ db/pool.js
@@ -1,13 +1,22 @@
 import { Pool } from 'pg';
 import 'dotenv/config';
 
-const pool = new Pool({
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-});
+const config = {};
+
+if (process.env.DATABASE_URL) {
+  config.connectionString = process.env.DATABASE_URL;
+  config.ssl = {
+    rejectUnauthorized: false,
+  };
+} else {
+  config.host = process.env.DATABASE_HOST;
+  config.port = process.env.DATABASE_PORT;
+  config.user = process.env.DATABASE_USER;
+  config.password = process.env.DATABASE_PASSWORD;
+  config.database = process.env.DATABASE_NAME;
+}
+
+const pool = new Pool(config);
 
 const dbModule = {
   query: (text, params) => pool.query(text, params),
