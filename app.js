72f725888a764694019c05d4ffaea07eaff9b099
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-11-12 14:47:12 -0500

    feat(static): serve static files from the public directory and add initial styles

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const __dirname = path.dirname(__filename);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: false }));
 
