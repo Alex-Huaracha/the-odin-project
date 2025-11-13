6bfd9ca2af1e423102d4b9e93ffda9931d68599a
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-11-13 11:46:04 -0500

    feat: configure session management with Prisma and Passport.js

--- app.js
+++ app.js
@@ -2,20 +2,52 @@ import express from 'express';
 import dotenv from 'dotenv';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import expressSession from 'express-session';
+import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import { PrismaClient } from '@prisma/client';
+import passport from 'passport';
+
+dotenv.config();
 
 const app = express();
 
+// Directory config (Fix ESM)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Session configuration
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    },
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  })
+);
+
+// Initialize Passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Views
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  console.log(req.session);
+  res.send('Server configured with Database Sessions.');
 });
 
 const PORT = process.env.PORT || 3000;
