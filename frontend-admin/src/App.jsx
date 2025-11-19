dfad799b99b33dd4ac763e5c706391b4c945f9b7
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-11-19 17:19:58 -0500

    feat: update App component to use actual Dashboard, CreatePost, and EditPost components

--- frontend-admin/src/App.jsx
+++ frontend-admin/src/App.jsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import ProtectedLayout from './components/ProtectedLayout';
+import Dashboard from './pages/Dashboard';
+import CreatePost from './pages/CreatePost';
+import EditPost from './pages/EditPost';
 
 function App() {
   return (
@@ -9,10 +12,9 @@ function App() {
         <Route path="/login" element={<Login />} />
 
         <Route element={<ProtectedLayout />}>
-          <Route
-            path="/dashboard"
-            element={<h2>Bienvenido al Dashboard 🏋️‍♂️</h2>}
-          />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/edit-post/:id" element={<EditPost />} />
         </Route>
 
         <Route path="*" element={<Navigate to="/login" />} />
