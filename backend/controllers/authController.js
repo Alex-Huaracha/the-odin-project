6ffb429b8f8adc9ea363d326da04a03084735218
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-11-20 13:29:46 -0500

    feat: implement user registration functionality in authController and update routes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -36,3 +36,28 @@ export const login = async (req, res) => {
     res.status(500).json({ error: 'Server error' });
   }
 };
+
+export const register = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const existingUser = await prisma.user.findUnique({ where: { username } });
+    if (existingUser) {
+      return res.status(400).json({ error: 'Username is already in use' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword,
+        role: 'USER',
+      },
+    });
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error registering user' });
+  }
+};
