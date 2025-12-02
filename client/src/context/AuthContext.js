0f16dab1d94b279c76d6847aa9c6076d2b735b36
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-12-01 20:16:06 -0500

    feat(auth): implement AuthContext and AuthProvider for user authentication management

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,10 @@
+import { createContext, useContext } from 'react';
+
+export const AuthContext = createContext();
+
+// Custom hook to easily use the context
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error('useAuth must be used within an AuthProvider');
+  return context;
+};
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
-import { process } from 'node:process';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
